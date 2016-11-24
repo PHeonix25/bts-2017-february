5c909374dd2d3997f9862a2a33dc5978d3a29987
Pat Hermens <p.hermens@coolblue.nl>
2016-11-24 21:24:22 +0100

    Updates dockerfile to do all the things

--- docker/seq/Docker.BuildAndRun.ps1
+++ docker/seq/Docker.BuildAndRun.ps1
@@ -1,11 +1,19 @@
-# Build the image (called blog, version 'latest')
-docker build . -t servercore-seq:latest 
+# Build the image 
+docker build . -t servercore-seq
 
 # Run an instance of the image in the background, opening port 5341 as well
-$container = docker run -d -t -p 5341:5341 servercore-seq:latest
+$hostport = 5341
+$container = docker run -d -t -p 5341:$hostport servercore-seq
+Write-Host "Docker container started:" $container
+
+# Find the IP of the running container
+$ip = docker inspect --format="{{.NetworkSettings.Networks.nat.IPAddress}}" $container
+Write-Host "Docker container IP:" $ip
 
 # Launch the browser so that we can check our work
-start 'http://localhost:5341/'
+$seq_url = 'http://'+ $ip + ':' + $hostport + '/'
+start $seq_url
+Write-Host "Browser launched, pointing at" $seq_url
 
 # Attach to the running container (for debug purposes)
 #docker attach $container
