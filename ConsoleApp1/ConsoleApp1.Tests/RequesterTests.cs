912c019c2fb8e3efc2d805d1acbdfae1c20685a8
Pat Hermens <p.hermens@coolblue.nl>
2016-11-24 22:11:21 +0100

    Adds unit tests -because why not

---
+++ ConsoleApp1/ConsoleApp1.Tests/RequesterTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass]
+    public class RequesterTests
+    {
+        [TestMethod]
+        public void RequestTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
