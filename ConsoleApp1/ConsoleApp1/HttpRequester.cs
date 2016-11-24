c16d2d98f8f494f10c219bc4bab33a4d40834c31
Pat Hermens <p.hermens@coolblue.nl>
2016-11-24 15:49:31 +0100

    Adds main logic for requests

---
+++ ConsoleApp1/ConsoleApp1/HttpRequester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    public class Requester
+    {
+        private readonly Uri _uri;
+
+        public Requester(Uri baseUri)
+        {
+            _uri = baseUri;
+        }
+
+        public async Task<T> Request<T>(string request) where T : class
+        {
+            using(var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = _uri;
+                    var response = await client.GetAsync(request);
+                    response.EnsureSuccessStatusCode();
+
+                    var streamResponse = await response.Content.ReadAsStreamAsync();
+                    var serializer = new XmlSerializer(typeof(T));
+                    var result = serializer.Deserialize(streamResponse) as T;
+
+                    return result;
+                }
+                catch(HttpRequestException e)
+                {
+                    Console.WriteLine($"HTTP Request exception: {e.Message}");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Exception generated during request: {ex}");
+                }
+            }
+            return default(T);
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using ConsoleApp1.DTOs;
 
 namespace ConsoleApp1
 {
-    class Program
+    static class Program
     {
+        const string LAST_FM_API_ENDPOINT = "http://ws.audioscrobbler.com/2.0/";
+        const string LAST_FM_API_KEY = "48308a6b32cf819c13aa999383c36985";
+        const string LAST_FM_MY_USERNAME = "pheonix25";
+
         static void Main(string[] args)
         {
+            MakeWebRequest().DisplaySomeStats();
+            Console.ReadLine();
         }
+
+        private static TopTracks MakeWebRequest()
+        {
+            var query = $"?method=user.gettoptracks&user={LAST_FM_MY_USERNAME}&api_key={LAST_FM_API_KEY}";
+            Console.WriteLine($"Querying: {query} from LastFM.");
+            
+            var requester = new Requester(new Uri(LAST_FM_API_ENDPOINT));
+            var result = requester.Request<LastFmResponse>(query).Result;
+            Console.WriteLine($"Request returned response code: {result.ResponseStatus}");
+            return result.Response;
+        }
+
+        private static void DisplaySomeStats(this TopTracks result)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Printing a list of my 10 most 'scrobbled' tracks");
+            Console.WriteLine("------------------------------------------------");
+            foreach(var track in result.Tracks.Take(10))
+                Console.WriteLine($"{track.Rank}:\t'{track.Name}' by {track.Artist.Name}.");
+            Console.WriteLine("------------------------------------------------");
+        }
+
+        // http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=pheonix25&api_key=48308a6b32cf819c13aa999383c36985
     }
 }
