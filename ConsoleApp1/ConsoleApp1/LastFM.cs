a1e5933c4e364744621917cdf69f584802023e9f
Pat Hermens <p.hermens@coolblue.nl>
2016-12-04 15:05:10 +0100

    Updates ConsoleApp to support Seq

---
+++ ConsoleApp1/ConsoleApp1/LastFM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ConsoleApp1.DTOs;
+
+using log4net;
+using log4net.Config;
+
+namespace ConsoleApp1
+{
+    static class LastFM
+    {
+        private static ILog _logger;
+        const string LAST_FM_API_ENDPOINT = "http://ws.audioscrobbler.com/2.0/";
+        const string LAST_FM_API_KEY = "48308a6b32cf819c13aa999383c36985";
+        const string LAST_FM_MY_USERNAME = "pheonix25";
+
+        static LastFM()
+        {
+            _logger = LogManager.GetLogger(typeof(LastFM));
+            XmlConfigurator.Configure();
+        }
+
+        public static TopTracks MakeWebRequest()
+        {
+            var query = $"?method=user.gettoptracks&user={LAST_FM_MY_USERNAME}&api_key={LAST_FM_API_KEY}";
+            _logger.Info($"Querying: {query} from LastFM.");
+
+            var requester = new Requester(new Uri(LAST_FM_API_ENDPOINT));
+            var result = requester.Request<LastFmResponse>(query).Result;
+
+            _logger.Info($"Request returned response code: {result.ResponseStatus}");
+
+            return result.Response;
+        }
+
+        public static IEnumerable<Track> PrintMyTopTracks(this TopTracks result, int trackCount = 10)
+        {
+            Console.WriteLine();
+            _logger.Info($"Printing a list of my {trackCount} most 'scrobbled' tracks");
+            Console.WriteLine("------------------------------------------------");
+
+            foreach (var track in result.Tracks.Take(trackCount))
+                _logger.Info($"{track.Rank}:\t'{track.Name}' by {track.Artist.Name}.");
+
+            Console.WriteLine("------------------------------------------------");
+            return result.Tracks.Skip(trackCount);
+        }
+
+        public static void PrintWarningIfThereTracksLeft(this IEnumerable<Track> tracks)
+        {
+            _logger.Warn($"There are {tracks.Count()} other tracks not shown.");
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Linq;
-
-using ConsoleApp1.DTOs;
 
 using log4net;
 using log4net.Config;
@@ -10,9 +7,6 @@ namespace ConsoleApp1
 {
     static class Program
     {
-        const string LAST_FM_API_ENDPOINT = "http://ws.audioscrobbler.com/2.0/";
-        const string LAST_FM_API_KEY = "48308a6b32cf819c13aa999383c36985";
-        const string LAST_FM_MY_USERNAME = "pheonix25";
         private static ILog _logger;
 
         static void Main(string[] args)
@@ -20,41 +14,20 @@ namespace ConsoleApp1
             _logger = LogManager.GetLogger(typeof(Program));
             XmlConfigurator.Configure();
 
+            LastFM.MakeWebRequest()
+                .PrintMyTopTracks()
+                .PrintWarningIfThereTracksLeft();
+
             try
             {
-                MakeWebRequest().DisplaySomeStats();
                 throw new DivideByZeroException();
             }
             catch(Exception ex)
             {
-                _logger.Fatal($"Something went wrong: {ex}");
+                _logger.Fatal($"We caught an exception: {ex}");
             }
 
             Console.ReadLine();
-        }
-
-        private static TopTracks MakeWebRequest()
-        {
-            var query = $"?method=user.gettoptracks&user={LAST_FM_MY_USERNAME}&api_key={LAST_FM_API_KEY}";
-            _logger.Info($"Querying: {query} from LastFM.");
-
-            var requester = new Requester(new Uri(LAST_FM_API_ENDPOINT));
-            var result = requester.Request<LastFmResponse>(query).Result;
-
-            _logger.InfoFormat("Request returned response code: {0}", result.ResponseStatus);
-
-            return result.Response;
-        }
-
-        private static void DisplaySomeStats(this TopTracks result, int trackCount = 10)
-        {
-            Console.WriteLine();
-            _logger.Info("Printing a list of my 10 most 'scrobbled' tracks");
-            Console.WriteLine("------------------------------------------------");
-            foreach(var track in result.Tracks.Take(trackCount))
-                _logger.Info($"{track.Rank}:\t'{track.Name}' by {track.Artist.Name}.");
-            Console.WriteLine("------------------------------------------------");
-            _logger.Warn($"There are {result.TotalTracks - trackCount} other tracks not shown above.");
         }
     }
 }
